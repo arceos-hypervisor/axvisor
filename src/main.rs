2d197056b5308c74b7160a1bd2869b96479e5949
aarkegz <aarkegz@gmail.com>
2025-04-21 23:50:57 +0800

    add logo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use std::println;
+
 #[macro_use]
 extern crate log;
 #[macro_use]
@@ -12,8 +14,27 @@ mod hal;
 mod task;
 mod vmm;
 
+const LOGO: &'static str = r#"
+       d8888            888     888  d8b
+      d88888            888     888  Y8P
+     d88P888            888     888
+    d88P 888  888  888  Y88b   d88P  888  .d8888b    .d88b.   888d888
+   d88P  888  `Y8bd8P'   Y88b d88P   888  88K       d88""88b  888P"
+  d88P   888    X88K      Y88o88P    888  "Y8888b.  888  888  888
+ d8888888888  .d8""8b.     Y888P     888       X88  Y88..88P  888
+d88P     888  888  888      Y8P      888   88888P'   "Y88P"   888
+"#;
+
+fn print_logo() {
+    println!();
+    println!("{}", LOGO);
+    println!();
+}
+
 #[unsafe(no_mangle)]
 fn main() {
+    print_logo();
+
     info!("Starting virtualization...");
 
     info!("Hardware support: {:?}", axvm::has_hardware_support());
