b1fbbec07da5a6e281fbb44a41620910f3007186
aarkegz <aarkegz@gmail.com>
2025-05-28 22:47:04 +0800

    impl `alloc_contiguous_frames` for axvisor_api

--- src/hal.rs
+++ src/hal.rs
@@ -132,3 +132,112 @@ pub(crate) fn enable_virtualization() {
         thread::yield_now();
     }
 }
+
+#[axvisor_api::api_mod_impl(axvisor_api::memory)]
+mod memory_api_impl {
+    use super::*;
+
+    extern fn alloc_frame() -> Option<HostPhysAddr> {
+        <AxMmHalImpl as AxMmHal>::alloc_frame()
+    }
+
+    extern fn alloc_contiguous_frames(num_frames: usize, frame_align_pow2: usize) -> Option<HostPhysAddr> {
+        arceos::modules::axalloc::global_allocator()
+            .alloc_pages(num_frames, PAGE_SIZE_4K << frame_align_pow2)
+            .map(|vaddr| <AxMmHalImpl as AxMmHal>::virt_to_phys(vaddr.into()))
+            .ok()
+    }
+
+    extern fn dealloc_frame(paddr: HostPhysAddr) {
+        <AxMmHalImpl as AxMmHal>::dealloc_frame(paddr)
+    }
+
+    extern fn phys_to_virt(paddr: HostPhysAddr) -> HostVirtAddr {
+        <AxMmHalImpl as AxMmHal>::phys_to_virt(paddr)
+    }
+
+    extern fn virt_to_phys(vaddr: HostVirtAddr) -> HostPhysAddr {
+        <AxMmHalImpl as AxMmHal>::virt_to_phys(vaddr)
+    }
+}
+
+#[axvisor_api::api_mod_impl(axvisor_api::time)]
+mod time_api_impl {
+    use super::*;
+    use axvisor_api::time::{CancelToken, Nanos, Ticks, TimeValue};
+
+    extern fn current_ticks() -> Ticks {
+        axhal::time::current_ticks()
+    }
+
+    extern fn ticks_to_nanos(ticks: Ticks) -> Nanos {
+        axhal::time::ticks_to_nanos(ticks)
+    }
+
+    extern fn nanos_to_ticks(nanos: Nanos) -> Ticks {
+        axhal::time::nanos_to_ticks(nanos)
+    }
+
+    extern fn register_timer(
+        deadline: TimeValue,
+        handler: alloc::boxed::Box<dyn FnOnce(TimeValue) + Send + 'static>,
+    ) -> CancelToken {
+        vmm::timer::register_timer(deadline.as_nanos() as u64, |t| handler(t))
+    }
+
+    extern fn cancel_timer(token: CancelToken) {
+        vmm::timer::cancel_timer(token)
+    }
+}
+
+#[axvisor_api::api_mod_impl(axvisor_api::vmm)]
+mod vmm_api_impl {
+    use super::*;
+    use axvisor_api::vmm::{InterruptVector, VCpuId, VMId};
+
+    extern fn current_vm_id() -> usize {
+        <AxVMHalImpl as AxVMHal>::current_vm_id()
+    }
+
+    extern fn current_vcpu_id() -> usize {
+        <AxVMHalImpl as AxVMHal>::current_vcpu_id()
+    }
+
+    extern fn vcpu_num(vm_id: VMId) -> Option<usize> {
+        vmm::with_wm(vm_id, |vm| vm.vcpu_num())
+    }
+
+    extern fn active_vcpus(vm_id: VMId) -> Option<usize> {
+        todo!("active_vcpus")
+    }
+
+    extern fn inject_interrupt(vm_id: VMId, vcpu_id: VCpuId, vector: InterruptVector) {
+        <AxVMHalImpl as AxVMHal>::inject_irq_to_vcpu(vm_id, vcpu_id, vector as usize).unwrap();
+    }
+
+    extern fn notify_vcpu_timer_expired(vm_id: VMId, vcpu_id: VCpuId) {
+        todo!("notify_vcpu_timer_expired")
+        // vmm::timer::notify_timer_expired(vm_id, vcpu_id);
+    }
+}
+
+#[axvisor_api::api_mod_impl(axvisor_api::arch)]
+mod arch_api_impl {
+    #[cfg(target_arch = "aarch64")]
+    extern fn hardware_inject_virtual_interrupt(irq: axvisor_api::vmm::InterruptVector) {
+        use axstd::os::arceos::modules::axhal;
+        axhal::irq::inject_interrupt(irq as usize);
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    extern fn read_vgicd_typer() -> u32 {
+        use axstd::os::arceos::modules::axhal::irq::MyVgic;
+        MyVgic::get_gicd().lock().get_typer()
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    extern fn read_vgicd_iidr() -> u32 {
+        use axstd::os::arceos::modules::axhal::irq::MyVgic;
+        MyVgic::get_gicd().lock().get_iidr()
+    }
+}
