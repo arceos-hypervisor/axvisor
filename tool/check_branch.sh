08719c73551fe901e0fb268547ec1862d0f6f6f3
baiqiaosen <baiqiaosen@gmail.com>
2025-06-19 20:11:18 -0600

    supplement boot-on-qemu

---
+++ tool/check_branch.sh
@@ -0,0 +1,9 @@
+cd crates/arceos && git checkout rk3588_jd4_qemu
+cd ../arm_vcpu && git checkout inject_interrupt_vgicv3_ivc
+cd ../arm_vgic && git checkout inject_interrupt_vgicv3
+cd ../axaddrspace && git checkout 4_level_paging
+cd ../axdevice && git checkout ivc
+cd ../axdevice_crates && git checkout inject_interrupt_ivc
+cd ../axhvc && git checkout ivc
+cd ../axvcpu && git checkout inject_interrupt_vgicv3_ivc
+cd ../axvm && git checkout ivc
