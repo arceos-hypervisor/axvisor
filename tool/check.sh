699dc18cff1f66a64c30fcb802c33b9ee4c8735f
baiqiaosen <baiqiaosen@gmail.com>
2025-06-30 00:35:17 -0600

    update

---
+++ tool/check.sh
@@ -0,0 +1,16 @@
+cd crates/arceos && git checkout rk3588_jd4_qemu
+cd -
+cd crates/axvm && git checkout ivc
+cd -
+cd crates/axvcpu && git checkout ivc
+cd -
+cd crates/arm_vcpu && git checkout ivc_and_4lpt
+cd -
+cd crates/axaddrspace && git checkout 4_level_paging
+cd -
+cd crates/axhvc && git checkout ivc
+cd -
+cd crates/axdevice && git checkout ivc
+cd -
+cd crates/axvmconfig && git checkout ivc
+cd -
