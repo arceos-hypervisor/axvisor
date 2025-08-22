3220895688ea9d09d44bc7068cd66835103ee948
hky1999 <976929993@qq.com>
2025-08-22 16:37:33 +0800

    [fix] improve patch at src/vmm/config.rs only for x86_64-x14sbi-linux

--- build.rs
+++ build.rs
@@ -477,11 +477,35 @@ fn gen_libos_configs() -> io::Result<()> {
     Ok(())
 }
 
+const BUILTIN_PLATFORMS: &[&str] = &[
+    "aarch64-bsta1000b",
+    "aarch64-qemu-virt",
+    "aarch64-raspi4",
+    "aarch64-rk3588j",
+    "riscv64-qemu-virt",
+    "x86_64-pc-oslab",
+    "x86_64-qemu-q35",
+    "x86_64-x14sbi-linux",
+];
+
+fn make_cfg_values(str_list: &[&str]) -> String {
+    str_list
+        .iter()
+        .map(|s| format!("{:?}", s))
+        .collect::<Vec<_>>()
+        .join(", ")
+}
+
 fn main() -> io::Result<()> {
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
 
     let platform = env::var("AX_PLATFORM").unwrap_or("".to_string());
     println!("cargo:rustc-cfg=platform=\"{}\"", platform);
+
+    println!(
+        "cargo::rustc-check-cfg=cfg(platform, values({}))",
+        make_cfg_values(BUILTIN_PLATFORMS)
+    );
 
     if platform != "dummy" {
         gen_linker_script(&arch, platform.as_str()).unwrap();
