1485dcd3ca3bde630cec3858d6ef6a1e2e1c9ad8
szy <songzhyo@qq.com>
2025-09-28 16:09:16 +0800

    fix no dtb_load_gpa error

--- src/vmm/images/mod.rs
+++ src/vmm/images/mod.rs
@@ -108,20 +108,22 @@ impl ImageLoader {
         // Load DTB image
         let vm_config = axvm::config::AxVMConfig::from(self.config.clone());
         if let Some(dtb_arc) = get_vm_dtb_arc(&vm_config) {
-            let dtb_slice: &[u8] = &dtb_arc;
-            debug!(
-                "DTB buffer addr: {:x}, size: {:#}",
-                self.dtb_load_gpa.unwrap(),
-                Byte::from(dtb_slice.len())
-            );
+            if let Some(dtb_load_gpa) = self.dtb_load_gpa {
+                let dtb_slice: &[u8] = &dtb_arc;
+                debug!(
+                    "DTB buffer addr: {:x}, size: {:#}",
+                    dtb_load_gpa,
+                    Byte::from(dtb_slice.len())
+                );
 
-            #[cfg(target_arch = "aarch64")]
-            update_fdt(
-                self.dtb_load_gpa.unwrap(),
-                NonNull::new(dtb_slice.as_ptr() as *mut u8).unwrap(),
-                dtb_slice.len(),
-                self.vm.clone(),
-            );
+                #[cfg(target_arch = "aarch64")]
+                update_fdt(
+                    dtb_load_gpa,
+                    NonNull::new(dtb_slice.as_ptr() as *mut u8).unwrap(),
+                    dtb_slice.len(),
+                    self.vm.clone(),
+                );
+            }
         }
 
         // Load BIOS image
