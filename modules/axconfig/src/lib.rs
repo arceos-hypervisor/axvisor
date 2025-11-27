7ac42bbc10f8b483355196e211d07bd0d02e7724
YanLien <YJQ980314@outlook.com>
2025-11-27 12:28:35 +0800

    feat(ci): add x86_64 NimbOS support and upgrade guest images to v0.0.20

--- modules/axconfig/src/lib.rs
+++ modules/axconfig/src/lib.rs
@@ -24,15 +24,21 @@ pub const TICKS_PER_SEC: usize = 100;
 #[doc = ""]
 pub mod devices {
     #[doc = " MMIO regions with format (`base_paddr`, `size`)."]
-    pub const MMIO_REGIONS: &[(usize, usize)] = &[];
+    pub const MMIO_REGIONS: &[(usize, usize)] = &[
+        (0xb000_0000, 0x1000_0000), // PCI config space
+        (0xfe00_0000, 0xc0_0000),   // PCI devices
+        (0xfec0_0000, 0x1000),      // IO APIC
+        (0xfed0_0000, 0x1000),      // HPET
+        (0xfee0_0000, 0x1000),      // Local APIC
+    ];
     #[doc = " End PCI bus number."]
-    pub const PCI_BUS_END: usize = 0;
+    pub const PCI_BUS_END: usize = 0xff;
     #[doc = " Base physical address of the PCIe ECAM space."]
-    pub const PCI_ECAM_BASE: usize = 0;
+    pub const PCI_ECAM_BASE: usize = 0xb000_0000     ;
     #[doc = " PCI device memory ranges."]
     pub const PCI_RANGES: &[(usize, usize)] = &[];
     #[doc = " Timer interrupt num (PPI, physical timer)."]
-    pub const TIMER_IRQ: usize = 26;
+    pub const TIMER_IRQ: usize = 0xf0;
     #[doc = " VirtIO MMIO regions with format (`base_paddr`, `size`)."]
     pub const VIRTIO_MMIO_REGIONS: &[(usize, usize)] = &[];
 }
@@ -41,17 +47,17 @@ pub mod devices {
 #[doc = ""]
 pub mod plat {
     #[doc = " Number of CPUs."]
-    pub const CPU_NUM: usize = 16;
+    pub const CPU_NUM: usize = 1;
     #[doc = " Platform family (deprecated)."]
     pub const FAMILY: &str = "";
     #[doc = " Kernel address space base."]
-    pub const KERNEL_ASPACE_BASE: usize = 0x0000_0000_0000;
+    pub const KERNEL_ASPACE_BASE: usize = 0xffff_8000_0000_0000;
     #[doc = " Kernel address space size."]
-    pub const KERNEL_ASPACE_SIZE: usize = 0xffff_ffff_f000;
+    pub const KERNEL_ASPACE_SIZE: usize = 0x0000_7fff_ffff_f000;
     #[doc = " No need."]
-    pub const KERNEL_BASE_PADDR: usize = 0x0;
+    pub const KERNEL_BASE_PADDR: usize = 0x20_0000   ;
     #[doc = " Base virtual address of the kernel image."]
-    pub const KERNEL_BASE_VADDR: usize = 0x8000_0000_0000;
+    pub const KERNEL_BASE_VADDR: usize = 0xffff_8000_0020_0000;
     #[doc = " Offset of bus address and phys address. some boards, the bus address is"]
     #[doc = " different from the physical address."]
     pub const PHYS_BUS_OFFSET: usize = 0;
